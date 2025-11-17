c5a90c3126bec013651d901c9e7b68eb313e0923
Alexej-Mich61 <spas-expert@yandex.ru>
2025-11-17 18:06:57 +0300

    10. исправлена ошибка со списком районов в формах

--- contracts_app/forms.py
+++ contracts_app/forms.py
@@ -1,7 +1,7 @@
 # contracts_app/forms.py
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Contract, AK
+from .models import Contract, AK, Work, District
 from django.forms import inlineformset_factory
 
 
@@ -36,13 +36,28 @@ class AKForm(forms.ModelForm):
     class Meta:
         model = AK
         fields = ['number', 'district', 'address']
+        widgets = {
+            'district': forms.Select(attrs={'class': 'form-select'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'number': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print("AKForm инициализирован, районов в queryset:", self.fields['district'].queryset.count())
+        # Загружаем ВСЕ районы (или можно фильтровать по региону позже)
+        self.fields['district'].queryset = District.objects.select_related('region').order_by('region__name', 'name')
+        self.fields['district'].label_from_instance = lambda obj: f"{obj.name} — {obj.region.name}"
 
 
 AKFormSet = inlineformset_factory(
-    Contract, AK,
+    Contract,
+    AK,
     form=AKForm,
+    fields=('number', 'district', 'address'),
     extra=1,
     can_delete=True,
+    can_delete_extra=True,
     min_num=0,
     max_num=500,
     validate_max=True
