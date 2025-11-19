cdc8dc3aaf669bf09c2ab864c8c8adcf43233750
Alexej-Mich61 <spas-expert@yandex.ru>
2025-11-19 16:59:44 +0300

    13. Стили, правки

--- contracts/settings.py
+++ contracts/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'contracts_app',
     'crispy_forms',
     'crispy_bootstrap5',
